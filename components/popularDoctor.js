d779ae707486ef1f7aed5f802e2db468c6a7a277
Nguyễn Hoài Duy <hoaidii.it@gmail.com>
2024-04-22 11:23:54 +0700

    update style for component PopularDoctor

--- components/popularDoctor.js
+++ components/popularDoctor.js
@@ -1,23 +1,9 @@
 import React from 'react';
-import { View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native';
+import { View, Text, Image, StyleSheet, TouchableOpacity, ScrollView, TouchableWithoutFeedback } from 'react-native';
 
-const PopularDoctor = ({title, hideSeeAll, image, name, role, rating }) => {
+const PopularDoctor = ({image, name, role, rating }) => {
   return (
     <View style={styles.container}>
-        <View style={styles.headline}>
-            <Text style={styles.title}>
-                {title}
-            </Text>
-
-            {
-                !hideSeeAll && (
-                    <TouchableOpacity>
-                        <Text style={{fontSize: 18}}>See All</Text>
-                    </TouchableOpacity>
-                )
-            }
-        </View>
-
         <View style={styles.doctorCart}>
             {/* image */}
             <Image source={image} style={styles.image} />
@@ -36,45 +22,32 @@ const PopularDoctor = ({title, hideSeeAll, image, name, role, rating }) => {
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flexDirection: 'column',
-    marginBottom: 16,
-    paddingHorizontal: 16,
-  },
 
-  headline:{
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignContent: 'space-between'
-  },
-
-  title:{
-    fontSize: 18,
-    fontWeight: '500',
-    lineHeight: 21.33,
-    color: 'rgba(51, 51, 51, 1)'    
-  },
 
   doctorCart:{
     width: 190, 
     height: 264,
-    marginTop: 0,
-    borderTopStartRadius: 12, 
-    borderTopEndRadius: 12,
-    alignItems: 'center'
+    marginRight: 15,
+    marginTop: 22,
+    borderRadius: 12,
+    alignItems: 'center',
+    backgroundColor:'rgba(255, 255, 255, 1)'
   },
 
   image: {
     width: '100%',
     height: 180,
-    borderTopWidth: 12,
-    borderBottomWidth: 2,
+    borderTopRightRadius: 12,
+    borderTopLeftRadius: 12,
+    borderBottomLeftRadius: 2,
+    borderBottomRightRadius: 2,
   },
 
   details: {
+    height: 55,
     alignItems: 'center',
     marginTop: 14,
-    flex: 1,
+    marginBottom: 22,
   },
 
   name: {
