fcd121d99bed84fa224fc8e96a46d66c6f247c3c
Nguyễn Hoài Duy <hoaidii.it@gmail.com>
2024-05-22 08:52:10 +0700

    create new headerComponent

---
+++ components/headerComponent.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { View, StyleSheet } from 'react-native'
+import BackArrowIcon from '../assets/icon/backArrowIcon'
+import TextComponent from '../components/textComponent'
+import { useNavigation } from '@react-navigation/native'
+
+const HeaderComponent = ({ title }) => {
+  const navigation = useNavigation();
+
+  return (
+    <View style={styles.header}>
+      <BackArrowIcon onPress={() => navigation.goBack()} />
+      <TextComponent style={styles.textHeader}>{title}</TextComponent>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  header: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 34,
+  },
+
+  textHeader: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    lineHeight: 21.33,
+    letterSpacing: -0.3,
+    color: 'rgba(34, 34, 34, 1)',
+    marginLeft: 19,
+  },
+})
+
+export default HeaderComponent
